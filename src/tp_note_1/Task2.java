c6f857daeb2ae02c854517da9140ae20cf2c6b15
seam virak <seamvirak.itc@gmail.com>
2019-12-20 21:28:56 +0700

    Task2

--- src/tp_note_1/Task2.java
+++ src/tp_note_1/Task2.java
@@ -1,7 +1,25 @@
 package tp_note_1;
 
-public class Task2 {
-   public static void mian(String[] args){
+import java.util.Scanner;
 
+public class Task2 {
+   public static void main(String[] args){
+       int minute;
+       int hour;
+       int second;
+
+       System.out.print("Input number of seconds: ");
+       Scanner sc = new Scanner(System.in);
+
+       int sec = Integer.parseInt(sc.nextLine());
+       hour = sec / 3600;
+       minute = (sec - (3600*hour)) /60 ;
+       second = (sec - (3600*hour) - (minute*60));
+
+//       hour= sec / 3600;
+//       minute = (sec % 3600) / 60;
+//       second = sec % 60;
+
+       System.out.print(" Time corresponding to " + sec +" seconds is " +hour +":" + minute+ ":" + second);
    }
 }
